da655dcebb09af837ba6e85a85137ba6968bd4fa
Ah0-90 <120452612+Ah0-90@users.noreply.github.com>
2023-03-07 09:54:12 +0530

    Add files via upload

---
+++ QuestionAnswers.java
@@ -0,0 +1,47 @@
+package com.vardaan.quiz;
+
+public class QuestionAnswers {
+
+    public static String question[] = {
+            "Which is the second largest search engine?",
+            "Which is the largest search engine?",
+            "Which company owned Kotlin Language? ",
+            "Which company owned Android?",
+            "Which of the following is owned by ‘Microsoft Corporation’?",
+            "Which company owned Linux?",
+            "This app is made by ____ IDE",
+            "Which of the following is not a programming language?",
+            "Which of the following is not a browser?",
+            "Youtube is a platform of "
+
+    };
+
+    public static String choice[][] ={
+            {"Google","Bing","Yahhoo","Instagram"},
+            {"Bing","Instagram","Yahhoo","Google"},
+            {"Microsoft Corporation","Google inc.","Google","JetBrains"},
+            {"JetBrains","Google inc.","Microsoft Corporation","Apple inc."},
+            {"Mac os","Android","Windows","Linux"},
+            {"Linus Torvalds","Google inc.","Microsoft Corporation","JetBrains"},
+            {"Android Studio","Eclipse","NetBeans","None of these"},
+            {"Java","Kotlin","Notepad","None of these"},
+            {"Youtube","Google","Yahhoo","Bing"},
+            {"Coding","App making","Both (a) and (b)","None of these"}
+
+    };
+
+    public static String correctAnswer[] ={
+            "Bing",
+            "Google",
+            "JetBrains",
+            "Google inc.",
+            "Windows",
+            "Linus Torvalds",
+            "Android Studio",
+            "Notepad",
+            "Youtube",
+            "None of these"
+
+    };
+
+}
